dcbc313b72bdbf958735b973dfa9eea106ef63f8
Ian Mess <ianmess247@gmail.com>
2023-11-03 14:56:11 -0700

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Ian Mess on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
