d50abc6883f4773521195159b15fee82ae3a4be0
Ian Mess <ianmess247@gmail.com>
2023-11-21 15:58:29 -0800

    Made improvments on the UI.

--- FitnessApp/HomeView.swift
+++ FitnessApp/HomeView.swift
@@ -8,35 +8,72 @@ import SwiftUI
 
 struct HourView: View {
     var body: some View {
-        GeometryReader { geometry in
-            Rectangle()
-                .stroke(Color.black, lineWidth: 3)
-                .frame(width: geometry.size.width / 1.25, height: 50)
-                .position(x: geometry.size.width / 2, y: geometry.size.height / 4)
-               
-        }
+        
+            HStack{
+
+                Rectangle()
+                    .stroke(Color.black, lineWidth: 2.5)
+                    .frame(width: UIScreen.main.bounds.width / 1.35, height: 50)
+            }
+        
     }
 }
 
-
-
 struct HomeView: View {
-    var hrs: Int = 5;
+    
+    var hours: Int = 17;
+    var backgroundColor: Color = Color(red: 0.4627, green: 0.8392, blue: 1.0)
     var body: some View {
-        GeometryReader { geometry in
-            VStack(spacing: 0) {
-                Text("Today is Sunday Nov. 19th, 2023.")
+                
+        
+        ZStack{
+            
+            backgroundColor
+                .ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                Text("Sunday,")
                     .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-               
-                ScrollView {
-                    VStack(spacing: 0) {
-                        ForEach(0 ..< hrs, id: \.self) { _ in
-                            HourView()
-                        }
-                    }
+                Text("Nov. 19th, 2023")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Spacer()
+                
+                
+                
+                
+                List(0 ..< hours, id: \.self) { index in
+                    let time = (index + 1) % 12
+                    
+                    Text("\(time == 0 ? 12 : time) \(index < 11 ? "am" : "pm")")
+                        .frame(height: 30)
+                    
                 }
+                .listStyle(InsetListStyle())
+                .frame(width: UIScreen.main.bounds.width / 1.1, height: UIScreen.main.bounds.height / 1.75)
+                .cornerRadius(5.0)
+                .padding()
+                
+                
+                
+                
+                
+                Spacer()
+                Text("Add Event:")
+                    .bold()
+                Button(action: {
+                    print("Button tapped!")
+                }) {
+                    Image(systemName: "calendar.badge.plus")
+                        .resizable()
+                        .scaledToFit()
+                }
+                
+                
+                Spacer()
             }
         }
+        
     }
 }
 
