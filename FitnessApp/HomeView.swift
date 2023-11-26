5b27f1f401b0b9f882b426e1f84287bf0b1c9ffc
Ian Mess <ianmess247@gmail.com>
2023-11-25 22:34:51 -0800

    Added events objects and displayed each event object in the events list

--- FitnessApp/HomeView.swift
+++ FitnessApp/HomeView.swift
@@ -7,20 +7,24 @@
 import SwiftUI
 
 struct Event {
+    var id = UUID()
     var name: String
-    var duration: TimeInterval
-    var time: Date
+    var time: Int
+    var duration: Double // hours
+    var color: Color?
 }
 
-struct HourView: View {
+struct EventView: View {
+    var name: String
+    var color: Color?
+    
     var body: some View {
-        
         ZStack{
             Rectangle()
-                .fill(Color.indigo)
+                .fill(color ?? Color.indigo)
                 .frame(width: UIScreen.main.bounds.width / 1.50, height: 30)
                 .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
-            Text("CLASS")
+            Text(name)
                 .foregroundColor(.white)
         }
        
@@ -29,6 +33,17 @@ struct HourView: View {
 }
 
 struct HomeView: View {
+    
+    let events = [
+        Event(name: "Clean bathroom", time: 9, duration: 1, color: .blue),
+        Event(name: "Do laundry", time: 10, duration: 1, color: .blue),
+        Event(name: "Dust", time: 10, duration: 1, color: .blue),
+        Event(name: "Vacuum", time: 10, duration: 1, color: .blue),
+        Event(name: "Go to dads", time: 14, duration: 1),
+        Event(name: "Go to dads", time: 15, duration: 1),
+        Event(name: "Visit mom", time: 17, duration: 1, color: .green),
+        Event(name: "Watch AOT with husband", time: 21, duration: 1, color: .yellow),
+    ]
     
     var hours: Int = 24;
     var backgroundColor: Color = Color(red: 0.4627, green: 0.8392, blue: 1.0)
@@ -44,10 +59,9 @@ struct HomeView: View {
                 Spacer()
                 Text("Sunday,")
                     .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                Text("Nov. 19th, 2023")
+                Text("Nov. 25th, 2023")
                     .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
                 Spacer()
-                
                 
                 
                 
@@ -58,9 +72,16 @@ struct HomeView: View {
                         
                         Text("\(time == 0 ? 12 : time) \(isPM ? "pm" : "am")")
                             .frame(height: 30)
-                        Spacer()
-                        HourView()
+                  
+                        VStack{
+                            ForEach(events, id: \.id){ event in
+                                if event.time == (index+1){
+                                    EventView(name: event.name, color: event.color)
+                                }
+                            }
+                        }
                     }
+                    .listRowSeparatorTint(.black)
                     
                 }
                 .listStyle(InsetListStyle())
